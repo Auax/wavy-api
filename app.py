289a693f5a72c067b31630df277c1efbd55f48ed
Ibai Farina del Olmo <auax@Ibais-Air.home>
2021-09-12 15:42:28 +0200

    backend

--- app.py
+++ app.py
@@ -2,4 +2,5 @@ from app_modules.main.views import socketio, app
 
 # You can edit the routes in the views.py file
 # Start app
-socketio.run(app, debug=True, port=5000)
+if __name__ == '__main__':
+    socketio.run(app, debug=True, port=5000)
