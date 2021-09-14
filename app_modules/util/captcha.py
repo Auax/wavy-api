a8c146b7f90e57026f6d8cda07f386ec7721a3a5
Ibai Farina <ibaifo8@gmail.com>
2021-09-14 22:36:23 +0200

    test

--- app_modules/util/captcha.py
+++ app_modules/util/captcha.py
@@ -5,6 +5,7 @@ import os
 def verify_captcha(token) -> [dict, bool]:
     data = {"secret": os.environ["captcha_secret"],
             "response": token}
+    print(data)
     headers = {'Content-type': 'application/json'}
     res = requests.post("https://www.google.com/recaptcha/api/siteverify",
                         data=data,
